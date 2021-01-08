cea7a64dde6482a72b7625f26460f8bc7f7cf636
Esteban Garcia Suarez <egarcia@flexiti.com>
2021-01-08 14:53:04 -0300

    Initial commit and test

---
+++ MultiCurrencyMoney/Domain/Dollar.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    public class Dollar
+    {
+        public Dollar(int amount)
+        {
+            Amount = amount;
+        }
+
+        public int Amount { get; set; }
+
+        public void Times(int multiplier)
+        {
+            Amount *=  multiplier;
+        }
+    }
+}
---
+++ MultiCurrencyMoney/UnitTests/Domain/DollarTests.cs
@@ -0,0 +1,21 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace UnitTests.Domain
+{
+    public class DollarTests
+    {
+        [Fact]
+        public void TestMultiplication()
+        {
+            var dollar = new Dollar(5);
+
+            dollar.Times(2);
+
+            Assert.Equal(10, dollar.Amount);
+        }
+    }
+}
